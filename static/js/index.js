3af74ffdcc7f98f34741995403c66d7b0436cbc1
Kelsey Richardson <kelseyrichardson6@gmail.com>
2020-10-18 13:51:54 -0500

    added market clusters to leaflet map

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,8 @@
 
-// Creating map object
+// Creating Leaflet map object with maker clusters
 var myMap = L.map("mapid", {
     center: [44.9778, -93.2650],
-    zoom: 5
+    zoom: 13
   });
   
   // Adding tile layer
@@ -15,4 +15,29 @@ var myMap = L.map("mapid", {
     accessToken: API_KEY
   }).addTo(myMap);
 
-  console.log(API_KEY);
+// Assemble API query URL
+var url = "http://127.0.0.1:5500/api/v1.0"
+
+// Grab the data with d3
+d3.json(url).then(function(response) {
+
+  // Create a new marker cluster group
+  var markers = L.markerClusterGroup();
+
+  // Loop through data
+  for (var i = 0; i < response.length; i++) {
+
+    // Check for location property
+    if (response[i].latitude, reponse[i].longitude) {
+
+      // Add a new marker to the cluster group and bind a pop-up - NEED TO ADJUST POPUP
+      markers.addLayer(L.marker([response[i].latitude, reponse[i].longitude])
+        .bindPopup(response[i].date + response[i].problem + response[i].resistance + response[i].forceAction));
+    }
+
+  }
+
+  // Add our marker cluster layer to the map
+  myMap.addLayer(markers);
+});
+
