e114a201cb1df5df99551ffb225e2152ed06d00e
餔餔 <52117569+willhwh@users.noreply.github.com>
2020-10-20 13:37:37 -0500

    cleaning

---
+++ database/queries/check.sql
@@ -0,0 +1,19 @@
+select * from city;
+
+select * from city_summary;
+
+select * from precinct;
+
+select * from precinct_summary;
+
+select * from neighborhood;
+
+select * from neighborhood_summary;
+
+select * from force_categories;
+
+select * from subject;
+
+select * from police_force;
+
+select * from "case";
