8e5a5269f4f016c2a4cc2a9d372211711eae6f0e
12wollmana <awollman2@gmail.com>
2020-10-22 15:50:35 -0500

    Prettify code.

--- static/js/index.js
+++ static/js/index.js
@@ -1,59 +1,65 @@
-const serverBaseURL = "http://127.0.0.1:5000"
-const apiBaseURL = `${serverBaseURL}/api`
-const apiCurrentVersion = "v1.0"
+const serverBaseURL = "http://127.0.0.1:5000";
+const apiBaseURL = `${serverBaseURL}/api`;
+const apiCurrentVersion = "v1.0";
 
 /**
  * Global variables for the state of the page.
  */
 let state = {
-  "pieCharts" : [],
-  "map" : {},
-  "year" : "",
-  "caseMarkers" : {}
-}
+  pieCharts: [],
+  map: {},
+  year: "",
+  caseMarkers: {},
+};
 
 /**
  * HTML elements on the page as d3 objects.
  */
-const elements =  {
-  divLoading : d3.select(".loading"),
-  divMap : d3.select("#mapid"),
-  divPieChartRace : d3.select(".chart--race"),
-  divChartBar : d3.select(".chart--bar"),
-  selectYear : d3.select("#selectYear"),
-  buttonApplySettings : d3.select("#buttonApplySettings"),
-  divChartRowList : d3.selectAll(".row__charts"),
-  divChartColRace : d3.select(".col__chart-race")
-}
+const elements = {
+  divLoading: d3.select(".loading"),
+  divMap: d3.select("#mapid"),
+  divPieChartRace: d3.select(".chart--race"),
+  divChartBar: d3.select(".chart--bar"),
+  selectYear: d3.select("#selectYear"),
+  buttonApplySettings: d3.select("#buttonApplySettings"),
+  divChartRowList: d3.selectAll(".row__charts"),
+  divChartColRace: d3.select(".col__chart-race"),
+};
 
 /**
- * This error will be thrown when 
+ * This error will be thrown when
  * there is no data to make a chart out of.
  */
-class NoDataError extends Error{
-  constructor(message){
+class NoDataError extends Error {
+  /**
+   * Creates an error for when there's no data
+   * to make a chart from.
+   * @param {string} message
+   * Message to display.
+   */
+  constructor(message) {
     super(message);
-    this.name = "NoDataError"
+    this.name = "NoDataError";
   }
 }
 
 /**
  * Sets a sleep timer.
- * 
+ *
  * From: https://stackoverflow.com/questions/33289726/combination-of-async-function-await-settimeout
- * 
+ *
  * @param {int} m Time in millseconds
  */
-const sleep = m => new Promise(r => setTimeout(r, m))
+const sleep = (m) => new Promise((r) => setTimeout(r, m));
 
 /**
  * Main application logic.
  */
-async function main(){
+async function main() {
   await showLoading();
   bindHandlers();
 
-  try{
+  try {
     state.map = makeMap();
 
     const streetLayer = makeStreetTileLayer();
@@ -61,8 +67,7 @@ async function main(){
 
     const availableYears = await loadAvailableYears();
     populateSelectYears(availableYears);
-  }
-  finally {
+  } finally {
     hideLoading();
   }
 }
@@ -72,33 +77,32 @@ main();
 /**
  * Binds all handlers to their elements.
  */
-function bindHandlers(){
+function bindHandlers() {
   elements.buttonApplySettings.on("click", onApplySettings);
 }
 
 /**
  * Handler for applying settings.
  */
-async function onApplySettings(){
+async function onApplySettings() {
   d3.event.preventDefault();
 
   await showLoading();
   hideChartRows();
   clearData();
 
-  try{
+  try {
     const selectElement = elements.selectYear;
     const selectedOption = selectElement.property("value");
     console.log(selectedOption);
 
-    if(selectedOption != "None"){
+    if (selectedOption != "None") {
       const selectedYear = selectedOption;
       await updateCasesByYear(selectedYear);
       state.year = selectedYear;
       showChartRows();
     }
-  }
-  finally {
+  } finally {
     hideLoading();
   }
 }
@@ -106,7 +110,7 @@ async function onApplySettings(){
 /**
  * Clears data from page.
  */
-function clearData(){
+function clearData() {
   state.year = "";
   clearCharts();
   state.map.removeLayer(state.caseMarkers);
@@ -115,34 +119,33 @@ function clearData(){
 /**
  * Clears all charts.
  */
-function clearCharts(){
+function clearCharts() {
   let pieCharts = state.pieCharts;
-  pieCharts.forEach(chart => chart.dispose());
+  pieCharts.forEach((chart) => chart.dispose());
   state.pieCharts = [];
 }
 
 /**
  * Populates the select years element with options.
- * @param {string[]} options 
+ *
+ * @param {string[]} options
  * The options to add to the element.
  */
-function populateSelectYears(options){
+function populateSelectYears(options) {
   const selectElement = elements.selectYear;
-  options.forEach(
-    option => {
-      const optionElement = selectElement.append("option");
-      optionElement.text(option);
-    }
-  );
+  options.forEach((option) => {
+    const optionElement = selectElement.append("option");
+    optionElement.text(option);
+  });
 }
 
 /**
  * Updates the page with cases from a specified year.
- * @param {int} year 
+ *
+ * @param {int} year
  * The year to load cases from.
  */
-async function updateCasesByYear(year){
-
+async function updateCasesByYear(year) {
   const allCasesByYear = await loadCasesByYear(year);
   console.log(allCasesByYear);
 
@@ -156,7 +159,7 @@ async function updateCasesByYear(year){
 /**
  * Hides the loading screen from the page.
  */
-function hideLoading(){
+function hideLoading() {
   const loadingElement = elements.divLoading;
   hideElements(loadingElement);
 }
@@ -164,7 +167,7 @@ function hideLoading(){
 /**
  * Shows the loading screen on the page.
  */
-async function showLoading(){
+async function showLoading() {
   const loadingElement = elements.divLoading;
   showElements(loadingElement);
   await sleep(300); // Wait for cover-up animation to finish
@@ -173,7 +176,7 @@ async function showLoading(){
 /**
  * Hides all charts from the page.
  */
-function hideChartRows(){
+function hideChartRows() {
   const chartRowElements = elements.divChartRowList;
   hideElements(chartRowElements);
 }
@@ -181,7 +184,7 @@ function hideChartRows(){
 /**
  * Shows all charts on the page.
  */
-function showChartRows(){
+function showChartRows() {
   const chartRowElements = elements.divChartRowList;
   showElements(chartRowElements);
 }
@@ -189,12 +192,12 @@ function showChartRows(){
 /**
  * Creates a map on the page.
  */
-function makeMap(){
+function makeMap() {
   const mapContainer = elements.divMap;
   // Creating Leaflet map object with maker clusters
   var myMap = L.map(mapContainer.node(), {
-    center: [44.9778, -93.2650],
-    zoom: 13
+    center: [44.9778, -93.265],
+    zoom: 13,
   });
 
   return myMap;
@@ -203,18 +206,19 @@ function makeMap(){
 /**
  * Creates a street tile layer for the map.
  */
-function makeStreetTileLayer(){
+function makeStreetTileLayer() {
   // Adding tile layer
   const streetsLayer = L.tileLayer(
-    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", 
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
     {
-      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      attribution:
+        "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
       tileSize: 512,
       maxZoom: 18,
       zoomOffset: -1,
       id: "mapbox/streets-v11",
-      accessToken: API_KEY
-   }
+      accessToken: API_KEY,
+    }
   );
 
   return streetsLayer;
@@ -223,13 +227,14 @@ function makeStreetTileLayer(){
 /**
  * Creates a marker cluster layer from a list of cases
  * to display on the map.
- * @param {any[]} cases 
+ *
+ * @param {any[]} cases
  * Cases to create the markers from.
  */
-function createCaseClustersMarkers(cases){
+function createCaseClustersMarkers(cases) {
   var markers = L.markerClusterGroup();
 
-  cases.forEach(caseInfo => {
+  cases.forEach((caseInfo) => {
     const currentCase = caseInfo.case;
 
     const longitude = currentCase.longitude;
@@ -237,7 +242,7 @@ function createCaseClustersMarkers(cases){
     const date = currentCase.date;
     const problem = currentCase.problem;
 
-    if(longitude && latitude){
+    if (longitude && latitude) {
       const marker = L.marker([latitude, longitude]);
       marker.bindPopup(
         `
@@ -257,10 +262,11 @@ function createCaseClustersMarkers(cases){
 
 /**
  * Loads all cases for the specified year from the database.
- * @param {int} year 
+ *
+ * @param {int} year
  * The year to load the cases for.
  */
-async function loadCasesByYear(year){
+async function loadCasesByYear(year) {
   const url = `${apiBaseURL}/${apiCurrentVersion}/year/${year}`;
   const allCases = await d3.json(url);
   return allCases;
@@ -269,7 +275,7 @@ async function loadCasesByYear(year){
 /**
  * Loads all available years from the database.
  */
-async function loadAvailableYears(){
+async function loadAvailableYears() {
   const url = `${apiBaseURL}/${apiCurrentVersion}/year`;
   const response = await d3.json(url);
   return response.availableYears;
@@ -277,25 +283,23 @@ async function loadAvailableYears(){
 
 /**
  * Generates a set of charts using amCharts.
- * 
+ *
  * https://www.amcharts.com/docs/v4/
- * 
- * @param {any[]} cases 
+ *
+ * @param {any[]} cases
  * The cases to generate the charts from.
  */
-function generateAmCharts(cases){
+function generateAmCharts(cases) {
   am4core.useTheme(am4themes_animated);
-  
-  try{
+
+  try {
     let racePieChart = generateRacePieChart(cases);
     showRacePieChartCard();
     state.pieCharts.push(racePieChart);
-  }
-  catch(error){
-    if(error instanceof NoDataError){
+  } catch (error) {
+    if (error instanceof NoDataError) {
       hideRacePieChartCard();
-    }
-    else{
+    } else {
       throw error;
     }
   }
@@ -304,7 +308,7 @@ function generateAmCharts(cases){
 /**
  * Shows the pie chart card on race.
  */
-function showRacePieChartCard(){
+function showRacePieChartCard() {
   let element = elements.divChartColRace;
   showElements(element);
 }
@@ -312,45 +316,44 @@ function showRacePieChartCard(){
 /**
  * Hides the pie chart card on race.
  */
-function hideRacePieChartCard(){
+function hideRacePieChartCard() {
   let element = elements.divChartColRace;
   hideElements(element);
 }
 
 /**
  * Hide one or more elements on the page.
- * @param {any} elements 
+ * @param {any} elements
  * Elements to hide as a d3 object.
  */
-function hideElements(elements){
+function hideElements(elements) {
   elements.classed("hidden", true);
 }
 
 /**
  * Shows one or more elements on the page.
- * @param {any} elements 
+ * @param {any} elements
  * Elements to hide as a d3 object.
  */
-function showElements(elements){
+function showElements(elements) {
   elements.classed("hidden", false);
 }
 
 /**
  * Creates a pie chart using amCharts.
  * This pie chart displays the subject's races.
- * 
- * @param {any[]} cases 
+ *
+ * @param {any[]} cases
  * The cases to generate the chart from.
  */
-function generateRacePieChart(cases){
+function generateRacePieChart(cases) {
   const chartElement = elements.divPieChartRace;
 
   const raceData = getRaceData(cases);
-  if(raceData.length > 0){
+  if (raceData.length > 0) {
     var chart = generateAmPieChart(chartElement, "count", "race");
     chart.data = getRaceData(cases);
-  }
-  else{
+  } else {
     throw new NoDataError("No race data.");
   }
 
@@ -359,25 +362,19 @@ function generateRacePieChart(cases){
 
 /**
  * Generates a amChart pie chart.
- * 
- * @param {any} parentElement 
+ *
+ * @param {any} parentElement
  * Parent HTML element as a d3 object to append the chart to.
- * 
- * @param {string} valueKey 
+ *
+ * @param {string} valueKey
  * Key within the data to bind to the pie value.
- * 
- * @param {string} categoryKey 
+ *
+ * @param {string} categoryKey
  * Key within the data to bind to the pie category.
  */
-function generateAmPieChart(
-  parentElement, 
-  valueKey, 
-  categoryKey
-  ){
-    // Create chart instance
-  var chart = am4core.create(
-    parentElement.node(), 
-    am4charts.PieChart);
+function generateAmPieChart(parentElement, valueKey, categoryKey) {
+  // Create chart instance
+  var chart = am4core.create(parentElement.node(), am4charts.PieChart);
 
   chart.legend = new am4charts.Legend();
   chart.legend.position = "right";
@@ -395,40 +392,37 @@ function generateAmPieChart(
 
 /**
  * Gets all the race data from a list of cases.
- * @param {any[]} cases 
+ *
+ * @param {any[]} cases
  * The cases to find the race data from.
  */
-function getRaceData(cases){
+function getRaceData(cases) {
   let raceCount = {};
-  for (let caseInfo of cases){
+  for (let caseInfo of cases) {
     const caseData = caseInfo["case"];
 
     const force = caseData["force"];
-    if(!force) break;
+    if (!force) break;
 
     const subject = force["subject"];
-    if(!subject) break;
+    if (!subject) break;
 
     race = subject["race"];
-    if(race){
-      if(raceCount[race] > 0){
+    if (race) {
+      if (raceCount[race] > 0) {
         raceCount[race]++;
-      }
-      else{
+      } else {
         raceCount[race] = 1;
       }
     }
   }
 
   let raceStats = [];
-  Object.entries(raceCount).forEach(
-    ([race, count]) => {
-      raceStats.push({
-        "race" : race,
-        "count" : count
-      });
-    }
-  );
+  Object.entries(raceCount).forEach(([race, count]) => {
+    raceStats.push({
+      race: race,
+      count: count,
+    });
+  });
   return raceStats;
 }
-
